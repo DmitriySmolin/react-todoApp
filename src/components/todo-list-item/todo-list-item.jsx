142bbd28d8d7c0d76651b88b672632c77124ab66
neox56 <neox56@gmail.com>
2020-01-24 13:26:53 +0400

    Реализован функционал удаления задач из списка задач

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -21,8 +21,9 @@ export default class TodoListItem extends Component {
       };
     });
   };
+
   render() {
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = "todo-list-item";
@@ -51,6 +52,7 @@ export default class TodoListItem extends Component {
         <button
           type="button"
           className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
         >
           <i className="fa fa-trash-o" />
         </button>
