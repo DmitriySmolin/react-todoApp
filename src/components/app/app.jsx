69a380e9a0f88ee5bb7732765ed33935a678566b
neox56 <neox56@gmail.com>
2020-01-25 22:42:07 +0400

    Реализована обработка формы для добавления новых задач

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -11,11 +11,19 @@ export default class App extends Component {
 
   state = {
     todoData: [
-      { label: "Drink Coffee", important: false, id: 1 },
-      { label: "Make Awesome App", important: true, id: 2 },
-      { label: "Have  a lunch", important: false, id: 3 }
+      this.createTodoItem("Drink Coffee"),
+      this.createTodoItem("Make Awesome App"),
+      this.createTodoItem("Have  a lunch")
     ]
   };
+  createTodoItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    };
+  }
   deleteItem = id => {
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex(el => el.id === id);
@@ -28,35 +36,64 @@ export default class App extends Component {
       };
     });
   };
-  addItem = text => {
-    //generate id
-    const newItem = {
-      label: text,
-      important: false,
-      id: this.maxId++
-    };
 
-    //add element in array
+  onAddItem = text => {
+    const newItem = this.createTodoItem(text);
+
     this.setState(({ todoData }) => {
-      // const newTodoData = [...todoData, newItem]
       const newTodoData = [newItem, ...todoData];
       return {
         todoData: newTodoData
       };
     });
   };
+
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex(el => el.id === id);
+
+    //1.update object
+    const oldItem = arr[idx];
+    const newItem = { ...oldItem, [propName]: !oldItem[propName] };
+
+    //2. construct new array
+    return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
+  }
+
+  onToggleDone = id => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, "done")
+      };
+    });
+  };
+
+  onToggleImportant = id => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, "important")
+      };
+    });
+  };
+
   render() {
     const { todoData } = this.state;
+    const doneCount = todoData.filter(el => el.done).length;
+    const todoCount = todoData.length - doneCount;
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
 
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
-        <TodoList todos={todoData} onDeleted={this.deleteItem} />
-        <ItemAddForm onAddItem={this.addItem} />
+        <TodoList
+          todos={todoData}
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}
+        />
+        <ItemAddForm onAddItem={this.onAddItem} />
       </div>
     );
   }
--- src/components/item-add-form/item-add-form.jsx
+++ src/components/item-add-form/item-add-form.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import "./item-add-form.css";
 export default class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const inputValue = this.state.label.trim();
+    if (inputValue === "") return;
+
+    this.props.onAddItem(inputValue);
+  };
+
   render() {
-    const { onAddItem } = this.props;
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => onAddItem("Hello World")}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          onKeyDown={this.onLabelChange}
+          placeholder="What needs to be done"
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
     );
   }
 }
