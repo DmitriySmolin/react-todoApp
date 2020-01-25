1e4b2b8deaf0adca90d53c66f3052c173b3a2b8b
neox56 <neox56@gmail.com>
2020-01-26 01:15:22 +0400

    Реализован поиск по списку задач

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./search-panel.css";
-const SearchPanel = () => {
-  const searchText = "Search";
-  const searchStyle = {
-    fontSize: "20px"
+
+export default class SearchPanel extends Component {
+  state = {
+    term: ""
   };
-  return (
-    <input
-      className="search-input"
-      placeholder={searchText}
-      style={searchStyle}
-    />
-  );
-};
-export default SearchPanel;
+
+  onSearchChange = e => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+    const searchText = "Search";
+    const searchStyle = {
+      fontSize: "20px"
+    };
+    return (
+      <input
+        className="search-input"
+        placeholder={searchText}
+        style={searchStyle}
+        value={this.state.term}
+        onChange={this.onSearchChange}
+      />
+    );
+  }
+}
